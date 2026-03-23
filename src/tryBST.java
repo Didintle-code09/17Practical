a7fd51f7524329f98edd9e5befc5131bc6ceda1e
Didintle Moratamotho <moratamothodidintle@gmail.com>
2026-03-23 14:18:46 +0200

    Define Node class

--- src/tryBST.java
+++ src/tryBST.java
@@ -1,2 +1,12 @@
 public class tryBST {
+    class tNode {
+        int key;
+        tNode left;
+        tNode right;
+        tNode(int item) {
+            key = item;
+            left = null;
+            right = null;
+        }
+    }
 }
