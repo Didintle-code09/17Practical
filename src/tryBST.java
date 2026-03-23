9bb0b07531054d367b1cf75e953a5fe8ad38bc4f
Didintle Moratamotho <moratamothodidintle@gmail.com>
2026-03-23 14:26:27 +0200

    Create isBST method

--- src/tryBST.java
+++ src/tryBST.java
@@ -9,4 +9,16 @@ public class tryBST {
             right = null;
         }
     }
+    boolean isBST() {
+        return isBSTUtil(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    boolean isBSTUtil(tNode node, int min, int max) {
+        if (node == null) {
+            return true;
+        }
+        if (node.key <= min || node.key >= max) {
+            return false;
+        }
+        return isBSTUtil(node.left, min, node.key) && isBSTUtil(node.right, node.key, max);
+    }
 }
